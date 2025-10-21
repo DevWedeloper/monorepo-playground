25da9937547aa71e41e03bf56334e7351df09e4c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-21 22:45:28 +0800

    chore: configure react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,18 @@
 import antfu from '@antfu/eslint-config'
+import tsParser from '@typescript-eslint/parser'
 import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
 export default antfu({
   formatters: true,
+  react: true,
+}, {
+  files: ['**/*.ts', '**/*.tsx', '**/*.jsx'],
+  languageOptions: {
+    parser: tsParser,
+  },
+  rules: {
+    'no-undef': 'off',
+  },
 }, {
   plugins: {
     'better-tailwindcss': eslintPluginBetterTailwindcss,
@@ -13,4 +23,6 @@ export default antfu({
     'better-tailwindcss/enforce-consistent-line-wrapping': ['off'],
     'better-tailwindcss/no-unregistered-classes': ['off'],
   },
+}, {
+  ignores: ['**/.angular/**'],
 })
