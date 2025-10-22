0a98548a175a6241fafa535f3486ebcce8b9866a
DevWedeloper <vicnathangabrielle@gmail.com>
2025-10-22 21:41:19 +0800

    chore: setup svelte testing library

--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -2,6 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [tailwindcss(), sveltekit()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test-setup.ts'],
+  },
+  resolve: {
+    conditions: mode === 'test' ? ['browser'] : [],
+  },
+}))
