15fcb50172ef3897e308581dbc5fd4488e740a0b
DevWedeloper <vicnathangabrielle@gmail.com>
2025-11-20 02:29:22 +0800

    chore: use @angular-eslint/eslint-plugin-template

--- apps/angular/eslint.config.mjs
+++ apps/angular/eslint.config.mjs
@@ -12,6 +12,7 @@ import {
 import betterTailwindcssConfig from '@playground/eslint-config/better-tailwindcss'
 import tsParser from '@typescript-eslint/parser'
 import eslintParserAngular from 'angular-eslint'
+import angularEslintTemplatePlugin from '@angular-eslint/eslint-plugin-template'
 
 export default combine(
   ignores(),
@@ -39,7 +40,12 @@ export default combine(
     languageOptions: {
       parser: eslintParserAngular.templateParser,
     },
+    plugins: {
+      '@angular-eslint/template': angularEslintTemplatePlugin,
+    },
     rules: {
+      ...angularEslintTemplatePlugin.configs.recommended.rules,
+      ...angularEslintTemplatePlugin.configs.accessibility.rules,
       'style/no-trailing-spaces': 'off',
       'style/indent': 'off',
       'style/no-multiple-empty-lines': 'off',
